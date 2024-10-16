ac6670c947e02ba66831de9102c5ee9948b3c6e4
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-10-17 02:30:42 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,22 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+    ]
+)
+def test_is_isogram_returns_correct_boolean_value(
+        test_input: str,
+        expected: bool
+) -> None:
+    assert is_isogram(test_input) == expected, (
+        f"Expected '{test_input}' should be "
+        f"'{not expected}' but got '{expected}'"
+    )
